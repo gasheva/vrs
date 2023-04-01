2cba8ca019cf5c6abcb147216784286193ce0081
Tanya Gasheva <tanya@eljur.team>
2023-04-01 17:29:20 +0500

    add ui components

---
+++ app/src/components/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button @click="$emit('click')" class="button" :class="color">{{text}}</button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    color: {
+      type: String,
+      default: 'green',
+      validator(val){
+        return ['green', 'green-pale'].includes(val);
+      }
+    }
+  },
+  emits: ['click'],
+}
+</script>
+
+<style scoped>
+.button{
+  padding: 10px 44px;
+  color: white;
+  font-size: 25px;
+  line-height: 25px;
+  border-radius: var(--radius-1);
+}
+
+.green{
+  background-color: var(--color-green);
+}
+.green-pale {
+  background-color: var(--color-gray-dark);
+}
+</style>
---
+++ app/src/components/TheError.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  {{error}}
+</div>
+</template>
+
+<script>
+export default {
+  name: "TheError",
+  props: {
+    error: {
+      type: String,
+      default: 'Something went wrong'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
