3bbe028fd26b2b792fb05612d03057e7e12d46ed
Tanya Gasheva <tanya@eljur.team>
2023-04-02 14:22:01 +0500

    add calendar

--- app/src/views/CalendarView.vue
+++ app/src/views/CalendarView.vue
@@ -1,13 +1,42 @@
 <template>
-
+<div class="calendar">
+  <FullCalendar :options='calendarOptions' />
+</div>
 </template>
 
 <script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import {fetchEvents} from '@/services/api';
 export default {
-  name: "CalendarView"
+  name: "CalendarView",
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin],
+        initialView: 'dayGridMonth',
+        events: [],
+      },
+      error: null,
+    }
+  },
+
+  async created() {
+    try{
+      this.events = await fetchEvents()
+          .map(event=>({start: new Date(event.startTime), title: event.title}));
+    } catch (err){
+      this.error = err?.message || err
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.calendar {
+  margin: 90px;
+}
 </style>
